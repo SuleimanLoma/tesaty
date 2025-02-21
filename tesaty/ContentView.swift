2ddfc9c0eedd1b256d5adea82b4c9d3488926c11
SuleimanLoma <suleimanloma@gmail.com>
2025-02-21 16:52:09 +0600

    Initial Commit

---
+++ tesaty/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tesaty
+//
+//  Created by Suleiman Loma on 21/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tesaty/tesatyApp.swift
@@ -0,0 +1,17 @@
+//
+//  tesatyApp.swift
+//  tesaty
+//
+//  Created by Suleiman Loma on 21/2/25.
+//
+
+import SwiftUI
+
+@main
+struct tesatyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
